0285d84606dc59a1c58126ba855938d9c837367b
Stephen Faure <faure.stephen@gmail.com>
2019-01-15 16:20:42 +0100

    Add --to-default option to merge_close script

--- merge_close.sh
+++ merge_close.sh
@@ -6,11 +6,13 @@ parameters()
     echo "-t/--ticket the ticket number (16XXX usually)"
     echo "-s/--sprint the sprint number (11, 12, etc.)"
     echo "-m/--machine the machine model (exc or 9200)"
+    echo "--to-default: the feature branch must be directly merged into the default branch"
     echo -h/--help to show this message
     exit 1
 }
 
 output="default.patch"
+to_default=false
 
 while [ $# -gt 0 ]
 do
@@ -32,6 +34,10 @@ do
             machine=$2
             shift 2
             ;;
+        --to-default)
+            to_default=true
+            shift 1
+            ;;
         *)
             echo Parametre {$1} inconnu
             parameters
@@ -50,35 +56,47 @@ hg pull -u
 
 default_branch_name="cv-default-${machine}"
 dev_branch_name="cv-${machine}-1.${sprint_num}-dev"
-branch_name="${dev_branch_name}-ticket-${ticket_num}"
+feature_branch_name="${dev_branch_name}-ticket-${ticket_num}"
+
+feature_commit_msg="Merge with ${feature_branch_name} / Fix \#${ticket_num}"
 
 echo
-echo "Closing and merging feature branch (${branch_name})..."
-hg up $branch_name -C
+echo "Closing and merging feature branch (${feature_branch_name})..."
+hg up $feature_branch_name -C
 hg commit --close-branch -m "Close Branch"
 
-hg up $dev_branch_name -C
-hg merge $branch_name
+if [ "$to_default" = false ]; then
+    hg up $dev_branch_name -C
+    hg merge $feature_branch_name
 
-if [ ! $? -eq 0 ]; then
-    echo "There are merge conflicts, fix them and finish by hand"
-    exit 1
+    if [ ! $? -eq 0 ]; then
+        echo "There are merge conflicts, fix them and finish by hand"
+        exit 1
+    fi
+
+    hg commit -m "$feature_commit_msg"
 fi
-
-hg commit -m "Merge with ${branch_name} / Fix #${ticket_num}"
 
 echo
-echo "Merging dev branch (${dev_branch_name}) in default branch..."
 hg up $default_branch_name -C
 
-hg merge $dev_branch_name
+if [ "$to_default" = true ]; then
+    hg merge $feature_branch_name
+else
+    echo "Merging dev branch (${dev_branch_name}) in default branch..."
+    hg merge $dev_branch_name
+fi
 
 if [ ! $? -eq 0 ]; then
     echo "There are merge conflicts, fix them and finish by hand"
     exit 1
 fi
 
-hg commit -m "Merge with ${dev_branch_name}"
+if [ "$to_default" = true ]; then
+    hg commit -m "$feature_commit_msg"
+else
+    hg commit -m "Merge with ${dev_branch_name}"
+fi
 
 hg push
 
